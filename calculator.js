13e4f3c9c5c57a4ff83cfe191f335d26a954e368
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-05-09 11:41:31 -0400

    Creates math functions

--- calculator.js
+++ calculator.js
@@ -2,6 +2,32 @@
 const calculationHistory = []
 
 // Operation Function
+function add(a, b) {
+  const result = a + b;
 
+  return result;
+}
+
+function subtract(a, b) {
+  const result = a - b;
+
+  return result;
+}
+
+function multiply(a, b) {
+  const result = a * b;
+
+  return result;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    console.log("Error: Cannot divide by zero");
+    return null;
+  }
+  const result = a / b;
+
+  return result;
+}
 
 // Logging calculation to history
